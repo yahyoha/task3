6dd1aa86815906153449a93d236209861721964a
Kay Fleischmann <k.fleischmann.extern@senec.com>
2022-12-31 04:20:16 +0100

    join hetzner data with mapping files

--- cloudbillingtool/hetzner_billing.py
+++ cloudbillingtool/hetzner_billing.py
@@ -2,9 +2,10 @@ import re
 import csv
 from datetime import datetime
 
-from pyspark.shell import spark
+from pyspark import rdd
+from pyspark.pandas import DataFrame
 from pyspark.sql.types import StructType, StringType
-
+import cloudbillingtool.mapping_file as mapping_file
 
 hetzner_schema = StructType() \
   .add("Type",StringType(),True) \
@@ -17,25 +18,51 @@ hetzner_schema = StructType() \
   .add("Price", StringType(), True) \
   .add("HetznerCostResourceID", StringType(), True)
 
-def transform_hetzner_bill(row):
-    return {
-        "Type": row.Type,
-        "Product": row.Product,
-        "Description": row.Description,
-        "StartDate": row.StartDate,
-        "EndDate": row.EndDate,
-        "Quantity": row.Quantity,
-        "UnitPrice": row.UnitPrice,
-        "HetznerCostResourceID":  # Todo: i cannot use a function here, why?
-            re.search(r'#[0-9]+', row.Description).group() if re.search(r'#[0-9]+', row.Description) else None
-    }
+
+def extract_tag(desc):
+    return re.search(r'#[0-9]+', desc).group() if re.search(r'#[0-9]+', desc) else None
 
 
-def load_files(files_location):
+def fix_date_format_for_hetzner(billing_date_hetzner):
+    if not billing_date_hetzner:
+        return ""
+    month, day, year = billing_date_hetzner.split('-')
+    return f"{month}/{day}/{year}"
+
+
+def load_files(spark, files_location) -> rdd :
     return\
         spark.read\
         .options(format='csv', escape="\"", header=False)\
         .schema(hetzner_schema)\
         .csv(files_location)\
         .rdd\
-        .map(transform_hetzner_bill)
+        .map(lambda row : {
+            "Type": row.Type,
+            "Product": row.Product,
+            "Description": row.Description,
+            "StartDate": fix_date_format_for_hetzner(row.StartDate),
+            "EndDate": fix_date_format_for_hetzner(row.EndDate),
+            "Quantity": row.Quantity,
+            "UnitPrice": row.UnitPrice,
+            "CostResourceID":  extract_tag(row.Description),
+            "CostResourceTag": ""
+        })
+
+
+def load_with_mapping(spark, hetzner_data, mapping_files_path):
+    hetzner_df: DataFrame = load_files(spark, hetzner_data).toDF();
+    type_mapping_df :DataFrame = mapping_file\
+        .load_mapping_file(spark, mapping_files_path+"/type_mapping.csv", mapping_file.type_schema ).toDF();
+
+    resource_mapping_df :DataFrame = mapping_file\
+        .load_mapping_file(spark, mapping_files_path+"/resource_mapping.csv", mapping_file.resource_schema ).toDF();
+
+    type_mapping_df.show()
+    resource_mapping_df.show()
+
+    joined = hetzner_df\
+        .join(type_mapping_df, hetzner_df["Type"] == type_mapping_df["Type"], "left") \
+        .join(resource_mapping_df, hetzner_df["CostResourceID"] == resource_mapping_df["CostResourceID"], "left")
+
+    return joined
--- tests/unit/hetzner_billing/test_hetzner_billing.py
+++ tests/unit/hetzner_billing/test_hetzner_billing.py
@@ -3,12 +3,17 @@ from unittest import mock
 from pathlib import Path
 
 from pandas import DataFrame
+from pyspark.sql import SparkSession
 
 import cloudbillingtool.hetzner_billing as hetzner_billing
+
+spark = SparkSession \
+    .builder \
+    .appName("sparkTest") \
+    .getOrCreate()
 
 
 class TestHetznerBilling(unittest.TestCase):
     def testHetznerBillingLoad(self):
-        table: DataFrame = hetzner_billing.load_files("tests/data/hetzner/*.csv").toDF()
-        table.show()
-        #self.assertEqual(len(table), 868)
+        hetzner_billing_with_tags = hetzner_billing.load_with_mapping(spark, "tests/data/hetzner/*.csv", "tests/data")
+        hetzner_billing_with_tags.show()
