7b011ace4b31de6d40252a037fa3c690eed97be5
Kay Fleischmann <k.fleischmann.extern@senec.com>
2023-01-03 14:34:59 +0100

    write data with header

--- cloudbillingtool.py
+++ cloudbillingtool.py
@@ -2,17 +2,17 @@
 # Example Usage: python3 cloudbillingtool.py tests/data/hetzner/*.csv * tests/data /tmp/cloudbillingtool_output/
 
 import sys
-import cloudbillingtool.all_billing
-from cloudbillingtool import all_billing
 from pyspark.sql import SparkSession
-import pyspark
-
 from pyspark import SparkContext
+import cloudbillingtool.azure_billing as azure_billing
+import cloudbillingtool.hetzner_billing as hetzner_billing
+
 sc =SparkContext()
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
+        print(sys.argv)
         print("Usage: cloudbillingtool.py <hetzner_data> <azure_data> <work_dir> <output_path>", file=sys.stderr)
         sys.exit(-1)
 
@@ -21,20 +21,19 @@ if __name__ == "__main__":
     work_dir = sys.argv[3]
     output_path = sys.argv[4]
 
+    print(sys.argv)
+
     # Initialize the spark context.
     spark = SparkSession\
     .builder\
     .appName("CloudBillingTool")\
     .getOrCreate()
 
-    bills = cloudbillingtool.all_billing.load_all_with_tags(spark, hetzner_data, azure_data, work_dir )
+    azure_billing_with_tags = azure_billing.load_files_with_mapping(spark, azure_data, work_dir)
+    hetzner_billing_with_tags = hetzner_billing.load_files_with_mapping(spark, hetzner_data,work_dir)
 
-    azure_billing_with_tags = cloudbillingtool.azure_billing.load_files_with_mapping(spark, azure_data, work_dir)
-    hetzner_billing_with_tags = cloudbillingtool.hetzner_billing.load_files_with_mapping(spark, hetzner_data,work_dir)
-
+    # combine azure with hetzner billing
     all_billing = azure_billing_with_tags.rdd.union(hetzner_billing_with_tags.rdd)
 
-    for row in all_billing.collect():
-        print(row)
-
-    all_billing.write.mode('overwrite').options(delimiter='\t').csv(output_path+"/all_bills")
+    # write to file
+    all_billing.toDF().write.mode('overwrite').options(delimiter='\t', header=True).csv(output_path+"/all_billing")
