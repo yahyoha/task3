5524b61d32b8c901929302619127ea1527b0a2b0
Kay Fleischmann <k.fleischmann.extern@senec.com>
2023-01-05 09:42:51 +0100

    Fix Testcases

--- tests/test_azure_billing.py
+++ tests/test_azure_billing.py
@@ -1,17 +1,24 @@
+import unittest
+import sys
+import os
 from pyspark.sql import SparkSession
+
+from cloudbillingtool import azure_billing
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 spark = SparkSession \
     .builder \
     .appName("sparkTest") \
     .getOrCreate()
 
-#
-# class TestAzureBilling(unittest.TestCase):
-#
-#     def testAzureBillingLoad(self):
-#         azure_billing_with_tags = azure_billing.load_files_with_mapping(spark, "tests/data/azure/*.csv", "tests/data")
-#         rows = azure_billing_with_tags.rdd.collect()
-#         for row in rows:
-#             print(row)
-#
-#         azure_billing_with_tags.write.mode('overwrite').options( delimiter='\t').csv("/tmp/cloudbillingtool/azure_data")
+
+class TestAzureBilling(unittest.TestCase):
+
+    def testAzureBillingLoad(self):
+        azure_billing_with_tags = azure_billing.load_files_with_mapping(spark, "tests/data/azure/*.csv", "tests/data")
+        rows = azure_billing_with_tags.rdd.collect()
+        for row in rows:
+            print(row)
+
+        azure_billing_with_tags.write.mode('overwrite').options( delimiter='\t').csv("/tmp/cloudbillingtool/azure_data")
--- tests/test_hetzner_billing.py
+++ tests/test_hetzner_billing.py
@@ -1,36 +1,43 @@
+import unittest
+import sys
+import os
 from pyspark.sql import SparkSession
+
+from cloudbillingtool import hetzner_billing
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 spark = SparkSession \
     .builder \
     .appName("sparkTest") \
     .getOrCreate()
 
-#
-# class TestHetznerBilling(unittest.TestCase):
-#
-#     def testHetznerBillingLoad(self):
-#         hetzner_billing_with_tags = hetzner_billing.load_files_with_mapping(spark, "tests/data/hetzner/*.csv", "tests/data")
-#         rows = hetzner_billing_with_tags.rdd.map( lambda x: x).collect()
-#         for row in rows:
-#             print(row)
-#
-#         hetzner_billing_with_tags.write.mode('overwrite').options( delimiter='\t').csv("/tmp/cloudbillingtool/hetzner_data")
-#
-#     def testRowTagExtraction(self):
-#         mapping_files_path = "tests/data"
-#
-#         resource_mapping_df = pd.read_csv(mapping_files_path + "/resource_mapping.csv", sep='\t')
-#         type_mapping_df = pd.read_csv(mapping_files_path + "/type_mapping.csv", sep='\t')
-#
-#         rowDescription='Cloud-Projekt "JJ" (01.12.2021 - 31.12.2021)'
-#         rowType='Cloud-Projekt "NBA"'
-#
-#         tags = list(set(hetzner_billing.find_tags_in_df(resource_mapping_df, "CostResourceID",
-#                                                hetzner_billing.extract_costresourceid(rowDescription)) +
-#                             hetzner_billing.find_tags_in_df(type_mapping_df, "Type", rowType))) + ['']
-#         expected = ['PROD', 'IT-OPS', 'TEST', 'DEV', ''];
-#
-#         tags.sort()
-#         expected.sort()
-#
-#         self.assertEqual(tags, expected )
+
+class TestHetznerBilling(unittest.TestCase):
+
+    def testHetznerBillingLoad(self):
+        hetzner_billing_with_tags = hetzner_billing.load_files_with_mapping(spark, "tests/data/hetzner/*.csv", "tests/data")
+        rows = hetzner_billing_with_tags.rdd.map( lambda x: x).collect()
+        for row in rows:
+            print(row)
+
+        hetzner_billing_with_tags.write.mode('overwrite').options( delimiter='\t').csv("/tmp/cloudbillingtool/hetzner_data")
+
+    def testRowTagExtraction(self):
+        mapping_files_path = "tests/data"
+
+        resource_mapping_df = pd.read_csv(mapping_files_path + "/resource_mapping.csv", sep='\t')
+        type_mapping_df = pd.read_csv(mapping_files_path + "/type_mapping.csv", sep='\t')
+
+        rowDescription='Cloud-Projekt "JJ" (01.12.2021 - 31.12.2021)'
+        rowType='Cloud-Projekt "NBA"'
+
+        tags = list(set(hetzner_billing.find_tags_in_df(resource_mapping_df, "CostResourceID",
+                                               hetzner_billing.extract_costresourceid(rowDescription)) +
+                            hetzner_billing.find_tags_in_df(type_mapping_df, "Type", rowType))) + ['']
+        expected = ['PROD', 'IT-OPS', 'TEST', 'DEV', ''];
+
+        tags.sort()
+        expected.sort()
+
+        self.assertEqual(tags, expected )
