38ba4c440a33c785e7f0ce8766799176543dedd3
Kay Fleischmann <k.fleischmann.extern@senec.com>
2023-01-06 21:21:38 +0100

    upload to synapse workspace

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudbillingtool',
+    version='1.9.3',
+    python_requires='>=3.6',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
