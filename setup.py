9f12d5e9704843542720c59e9f125d8643f00844
Kay Fleischmann <k.fleischmann.extern@senec.com>
2023-01-07 22:12:44 +0100

    Add build nr

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import time
+
 from setuptools import setup, find_packages
 
+# Get the current time and date
+current_time = time.gmtime()
+
+# Format the time and date as a string
+build_number = time.strftime("%y%m%H%M%S", current_time)
+
 setup(
-    name='cloudbillingtool',
+    name='cloudbillingtool-{}'.format(build_number),
     version='1.9.6',
     python_requires='>=3.6',
     packages=find_packages(exclude=('tests', 'docs'))
