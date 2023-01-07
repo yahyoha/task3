e7839d66b2cf50de05d6d4547561a14e1b4b11ba
Kay Fleischmann <k.fleischmann.extern@senec.com>
2023-01-07 08:40:38 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudbillingtool',
-    version='1.9.4',
+    version='1.9.5',
     python_requires='>=3.6',
     packages=find_packages(exclude=('tests', 'docs'))
 )
