a1501b1c0bfa11bfcc654943156e98625342c2b1
MD IBRAHIM <md.ibrahim210@gmail.com>
2023-01-18 15:16:36 +0100

    updated ci pipeline

--- upload_grafana_dashboard.py
+++ upload_grafana_dashboard.py
@@ -4,11 +4,8 @@ import requests
 import json
 
 
-#this will upload the dashboard to grafana
-def upload_dashboard(dashboard_path):
-    # Get Grafana API URL and API key from GitLab environment variables
-    api_url = os.environ['GRAFANA_API_URL']
-    api_key = os.environ['GRAFANA_API_KEY']
+# this will upload the dashboard to grafana
+def upload_dashboard(dashboard_path, api_url, api_key):
 
     # Set headers for API call
     headers = {
@@ -31,11 +28,17 @@ def main():
     parser = argparse.ArgumentParser(description='Upload Grafana dashboard')
     parser.add_argument('--dashboard_dir', type=str, help='Source directory')
     parser.add_argument('--dashboard_name', type=str, help='Dashboard name')
+    parser.add_argument('--grafana_api', type=str, help='URL Api for Grafana')
+    parser.add_argument('--grafana_key', type=str, help='Key for Grafana')
 
     # Parse the command-line arguments
     args = parser.parse_args()
     dashboard_path = args.dashboard_dir + args.dashboard_name
-    upload_dashboard(dashboard_path)
+    api_url = args.grafana_api
+    api_key = args.grafana_key
+
+    upload_dashboard(dashboard_path, api_url, api_key)
+
 
 if __name__ == '__main__':
     main()
