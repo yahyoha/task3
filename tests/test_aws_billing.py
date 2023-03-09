822ce918459b2b08b50027464f2be2d59cdda8c5
Jonan <jonan.stein@web.de>
2023-03-09 14:37:42 +0700

    added aws test data

---
+++ tests/test_aws_billing.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, concat_ws
+from cloudbillingtool import aws_billing
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+spark = SparkSession \
+    .builder \
+    .appName("sparkTest") \
+    .getOrCreate()
+
+
+class TestAwsBilling(unittest.TestCase):
+
+    def testAwsBillingLoad(self):
+        aws_billing_with_tags = aws_billing.load_files_with_mapping(spark, "tests/data/aws/*.csv", "tests/metadata")
+
+        output_df = aws_billing_with_tags \
+            .withColumn("CostResourceTag", concat_ws(";", col("CostResourceTag"))) \
+            .withColumn("ProductTag", concat_ws(";", col("ProductTag")))
+
+        for row in output_df.collect():
+            print(row)
