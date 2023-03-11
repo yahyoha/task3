b7c0c0ab3a8f316aef623d5e6bfcdca28dcbe4aa
Jonan <jonan.stein@web.de>
2023-03-11 21:41:32 +0700

    Using new AWS CAU format, matching csv headers to Cloudbillingtool format

--- cloudbillingtool/aws_billing.py
+++ cloudbillingtool/aws_billing.py
@@ -6,62 +6,36 @@ from pyspark.sql.functions import col, lit, to_date, explode
 from . import helper
 
 
-# EXAMPLE
-# INPUT
-#"Service","EC2-Instances($)","EC2-Other($)","Tax($)","EC2-ELB($)","Support (Developer)($)","VPC($)","CloudWatch($)","Greengrass($)","S3($)","IoT($)","IoT Device Management($)","Lambda($)","Glue($)","Cognito($)","DynamoDB($)","SNS($)","Key Management Service($)","CloudShell($)","Service Catalog($)","Total costs($)"
-#"Service total","1771.6043260751","1240.6996371889","757.57","346.9232428487","290","231.528","89.9016232584","9.54","6.2471532432","0.8051679","0.0045279","0.0000836146","0","0","0","0","0","0","0","4744.8237620289"
-#"2022-03-01","3.4704","4.4454342454","53.33","","29","","0","0.36","0.0007563993","0.00251856","0.0000108","0.0000001891","0","0","0","0","","","","90.6091201938"
-
-## OUTPUT
-## Provider: AWS
-## Type: EC2-Instances($)
-## Costs:
-
-
-def transform_columns_to_rows(row):
-    rows = []
-    for idx, col_name in enumerate(row.__fields__):
-        if idx == 0: # skip first column
-            continue
-        value = getattr(row, col_name)
-        rows.append(Row(Product=col_name.rstrip("($)"), Costs=value, Date=row[0]))
-    return rows
-
-
 def load_files(spark, aws_data, work_folder ) -> rdd :
     resource_mapping_df = pd.read_csv(work_folder+"/resource_mapping.csv", sep='\t')
     type_mapping_df = pd.read_csv(work_folder+"/type_mapping.csv", sep='\t')
-
-    df = \
+    
+    return\
         spark.read\
-        .options(format='csv', escape="\"", header=True,  inferSchema=True) \
-        .csv(aws_data) \
-        .alias("aws_df")
-
-    new_rdd = df.rdd.flatMap(create_multiple_rows)
-
-    for row in new_rdd.collect():
-        print(row)
-
-    pass
-
-        # .withColumn("effectivePrice",col("effectivePrice").cast("decimal(12,8)")) \
-        #     .withColumn("quantity", col("quantity").cast("decimal(12,8)"))\
-        #     .withColumn("costInBillingCurrency", col("costInBillingCurrency").cast("decimal(12,8)"))\
-        #     .withColumn("unitPrice", col("unitPrice").cast("decimal(12,8)")) \
-        #     .rdd \
-        #     .map(lambda row: {
-        #         "Provider": "aws",
-        #         "Type":  "",  # Missing
-        #         "Costs": row.Costs,
-        #         "UnitPrice": "", # Missing
-        #         "Quantity": "", # Missing
-        #         "Product": row.Product,
-        #         "Date": row.Date,
-        #         "CostResourceID": "",
-        #         "CostResourceTag": "",
-        #         "ProductTag": ""
-        #   })
+        .options(format='csv', escape="\"", header=True)\
+        .withColumnRenamed("lineItem/UnblendedCost","UnblendedCost")\
+        .csv(aws_data)\
+        .rdd \
+        .map(lambda row: {
+            "Provider": "aws",
+            "Type":  "",  # Missing in Azure
+            "Costs": row.lineItem/UnblendedCost,
+            "UnitPrice": row.lineItem/UnblendedRate,
+            "Quantity": row.lineItem/UsageAmount,
+            "Product": row.lineItem/ProductName,
+            "Date": row.bill/BillingPeriodStartDate,
+            "CostResourceID": row.lineItem/ResourceId,
+            # "CostResourceTag": list(set(
+            #     [""] +
+            #     # no TypeMapping for Azure
+            #     # only mapping for CostRsourceId
+            #     list(resource_mapping_df.loc[resource_mapping_df['CostResourceID'] \
+            #          .str.contains(row.ResourceId)]['CostResourceTag'])
+            #     # filter resourceMapping for costresourceid
+            # )),
+            # "ProductTag": list(set([""]+resource_mapping_df.loc[resource_mapping_df['CostResourceID'].str.contains(
+            #     helper.extract_costresourceid(row.ProductName))]['ProductTag']))
+      })
 
 
 def load_files_with_mapping(spark, aws_data, metadata_folder):
@@ -70,7 +44,7 @@ def load_files_with_mapping(spark, aws_data, metadata_folder):
         load_files(spark, aws_data, metadata_folder+"/mappingfiles")\
         .toDF()\
         .alias("aws_df") \
-
+    
     # joined_with_tags = aws_df \
     #     .select(lit("azure").alias("Provider"),
     #             col("azure_df.Type"),
